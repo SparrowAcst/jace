b03c0fc201b6c49e6ba5233d8d365bfc762c71a9
boldak <boldak.andrey@gmail.com>
2023-02-27 17:51:42 +0200

    add harvest-controller

--- config/portal.js
+++ config/portal.js
@@ -23,7 +23,8 @@ module.exports = {
             
             "/api/md": require("../routes/api-md"),
             "/api/script": require("../jace-dps"),
-            "/api/data": require("../sync-data").router
+            "/api/data": require("../sync-data").router,
+            "/api/controller": require("../harvest-controller")
         
         },
 
