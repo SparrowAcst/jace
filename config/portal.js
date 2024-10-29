27a12bca9719f020e2d92f6d988c65fa00625a33
boldak <boldak.andrey@gmail.com>
2024-10-29 12:12:38 +0200

    initial config plugins

--- config/portal.js
+++ config/portal.js
@@ -23,13 +23,11 @@ module.exports = {
         plugins: {
             
             "/api/md": require("../routes/api-md"),
-            // "/api/script": require("../jace-dps"),
-            // "/api/data": require("../sync-data").router,
-            "/api/controller": require("../harvest-controller"),
-            "/api/ade-report": require("../report-builder"),
-            "/api/ade-lms": require("../ade-lms"),
-            "/api/patient-story": require("../ade-patient-story"),
-        
+            // "/api/controller": require("../harvest-controller"),
+            // "/api/ade-report": require("../report-builder"),
+            // "/api/ade-lms": require("../ade-lms"),
+            // "/api/patient-story": require("../ade-patient-story"),
+            "/api/controller": require("../ade-clinic"),
         },
 
 
